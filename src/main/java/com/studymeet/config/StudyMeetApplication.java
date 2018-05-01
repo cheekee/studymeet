141105e57740fc391055f874116bf503a711c2d4
cheekee <cheekee@cheekeeui-MacBook-Pro.local>
2018-05-01 17:28:16 +0900

    set jsp viewer, mvc mapping

--- src/main/java/com/studymeet/config/StudyMeetApplication.java
+++ src/main/java/com/studymeet/config/StudyMeetApplication.java
@@ -1,21 +1,15 @@
-package com.studymeet;
+package com.studymeet.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@RestController
+@ComponentScan(basePackages = "com.studymeet")
 public class StudyMeetApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudyMeetApplication.class, args);
-    }
-
-    @RequestMapping("/")
-    String helloWorld () {
-        return "hello~world";
     }
 
 }
---
+++ src/main/java/com/studymeet/user/web/UserController.java
@@ -0,0 +1,20 @@
+package com.studymeet.user.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        System.out.println("AAa");
+        return "user/loginForm";
+    }
+
+}
